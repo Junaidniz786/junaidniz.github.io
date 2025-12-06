0c1e238907bba37ebc497bc82adffe26fcc559e5
Junaidniz786 <Junaid20305@gmail.com>
2025-12-06 23:41:09 +0500

    Update User-Agent and PHPSESSID in headers

--- api/tempotp.js
+++ api/tempotp.js
@@ -49,12 +49,12 @@ module.exports = async (req, res) => {
 
   const headers = {
     "User-Agent":
-      "Mozilla/5.0 (Linux; Android 15; V2423 Build/AP3A.240905.015.A2_D1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.7390.122 Mobile Safari/537.36",
+      "Mozilla/5.0 (Linux; Android 15; V2423 Build/AP3A.240905.015.A2_D1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.7444.102 Mobile Safari/537.36",
     Accept: "application/json, text/javascript, */*; q=0.01",
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=qk55hsbh0hd0otacldrpuagaao",
+    Cookie: "PHPSESSID=qcfeob5njiqsshemlq6s6epgj4",
   };
 
   let url;
