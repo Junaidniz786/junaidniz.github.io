5d58c718ae24d8914628b91dff34a2201538bb5f
Junaidniz786 <Junaid20305@gmail.com>
2025-12-12 07:40:50 +0500

    Update tempotp.js

--- api/tempotp.js
+++ api/tempotp.js
@@ -54,7 +54,7 @@ module.exports = async (req, res) => {
     "X-Requested-With": "XMLHttpRequest",
     "Accept-Encoding": "gzip, deflate",
     "Accept-Language": "en-US,en;q=0.9",
-    Cookie: "PHPSESSID=hkedt877fq7umue5shk3vlgao9",
+    Cookie: "PHPSESSID=g7if2u6b30a4g0josdnbhtvl1f",
   };
 
   let url;
